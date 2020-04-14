84acf4385be10ac2c1439146bbc23a371f81b53c
Richard Liu <bluetoliu@gmail.com>
2020-04-14 18:00:39 +1000

    Login Navi

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import * as Font from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
+import { LoginScreen } from './screens/LoginScreen';
 import BottomTabNavigator from './navigation/BottomTabNavigator';
 import useLinking from './navigation/useLinking';
 
@@ -24,7 +24,7 @@ export default function App(props) {
         SplashScreen.preventAutoHide();
 
         // Load our initial navigation state
-        setInitialNavigationState(await getInitialState());
+        //setInitialNavigationState(await getInitialState());
 
         // Load fonts
         await Font.loadAsync({
@@ -49,8 +49,9 @@ export default function App(props) {
       <View style={styles.container}>
         {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
         
-        <NavigationContainer ref={containerRef} initialState={initialNavigationState}>
-          <Stack.Navigator> 
+        <NavigationContainer ref={containerRef} initialState={initialNavigationState}> 
+          <Stack.Navigator>  
+            <Stack.Screen name="Home" component={LoginScreen} />
             <Stack.Screen name="Root" component={BottomTabNavigator} />
           </Stack.Navigator>
         </NavigationContainer>

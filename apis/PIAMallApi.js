a500f0aa7241cd9621ca201e3c4481fa5d2554dc
Richard Liu <bluetoliu@gmail.com>
2020-04-29 10:11:48 +1000

    tiny

--- apis/PIAMallApi.js
+++ apis/PIAMallApi.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const PIAMallApi = axios.create({
-    baseURL: 'http://192.168.46.129:7150/en/'
+    //baseURL: 'http://192.168.46.129:7150/en/',
+    baseURL: 'http://172.17.0.1:7150/en/'
 })
 
 
--- screens/Mall/HomeScreen.js
+++ screens/Mall/HomeScreen.js
@@ -117,10 +117,8 @@ function handleHelpPress() {
   WebBrowser.openBrowserAsync(
     'https://docs.expo.io/versions/latest/get-started/create-a-new-app/#making-your-first-change'
   );
-}
-
-const SCREEN_WIDTH = Dimensions.get('window').width;
-const SCREEN_HEIGHT = Dimensions.get('window').height;
+} 
+const SCREEN_WIDTH = Dimensions.get('window').width; 
 
 const styles = StyleSheet.create({
   safe_container: {

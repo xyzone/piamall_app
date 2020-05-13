300809e8c82dd338abc1fe4bec1c54fc9a6efabb
Richard Liu <bluetoliu@gmail.com>
2020-05-13 23:10:46 +1000

    ngrok

--- apis/PIAMallApi.js
+++ apis/PIAMallApi.js
@@ -7,8 +7,9 @@ import { capPostData } from '../components/GeneralFunctions'
 
 
 export const PIAMallApi = axios.create({
-    baseURL: 'http://192.168.46.129:7150/en/'
-    //baseURL: 'http://172.17.0.1:7150/en/'
+    //baseURL: 'http://192.168.46.129:7150/en/'
+    //baseURL: 'http://127.0.0.1:7150/en/'test
+    baseURL: 'https://22926e15.ngrok.io/en/'
 })
 
 
